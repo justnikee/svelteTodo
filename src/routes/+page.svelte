64943aabb29c9f26bbcfbc95a173cd46e90912eb
justnikee <hk5561111212@gmail.com>
2023-09-04 00:21:22 +0530

    todo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Todo from './Todo.svelte';
+</script>
+
+<svelte:head>
+	<title>Todo App</title>
+	<meta name="description" content="Todo App" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome"> Todo App </span>
+	</h1>
+	<Todo />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+</style>
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,32 @@
+<script>
+	let todoItem = '';
+
+	let todoItems = [{ text: '', status: '' }];
+
+	function addTodo() {
+		todoItems = [...todoItems, { text: todoItem, status: false }];
+		localStorage.setItem('todo', JSON.stringify(todoItems));
+		newItem: '';
+	}
+
+	function removeTodo(index) {
+		todoItems.splice(index, 1);
+		todoItems = todoItems;
+	}
+</script>
+
+<div>
+	<input bind:value={todoItem} name="todo" type="text" placeholder="add todo" />
+	<button on:click={addTodo}>add</button>
+
+	<div>
+		{#each todoItems as item, index}
+			<input type="checkbox" bind:checked={item.status} />
+			<span class:checked={item.status}>{item.text}</span>
+			<span on:click={() => removeTodo(index)}>❌</span>
+		{/each}
+	</div>
+</div>
+
+<style>
+</style>
